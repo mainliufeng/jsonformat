d61e0dccff7e7ae899a4fbd7aec91796d6151d2d
刘峰 <mainliufeng@gmail.com>
2017-06-14 18:49:54 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='jsonformat',
-    version='0.1',
-    packages=find_packages(),
+    version='1.0',
+    packages=['scripts'],
     include_package_data=True,
     install_requires=[
         'Click',
     ],
     entry_points='''
         [console_scripts]
-        jsonformat=jsonformat:format
+        jsonformat=scripts.jsonformat:format
     ''',
 )
